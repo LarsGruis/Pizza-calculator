81ae002284c53be6a3d39a4acb8706126118d240
Lars Gruis <99047036@mydavinci.nl>
2019-02-06 14:34:56 +0100

    Javascript: De prijs wordt nu opgeteld als je op een topping klikt. HTML CSS: Kleine style veranderingen

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@
 
 pizza_list = ["Margeritha","Shoarma","Kaas","Salami","Hawai"];
 pizza_prices = [8, 9, 8.50, 8, 7.50];
+allPrices = [];
 
 var container = document.getElementById("pizzas");
  
@@ -59,17 +60,23 @@ function hidePizzaList($id) {
         pizza_name.innerHTML = 'Uw gekozen pizza: ' + pizza_list[$id];
         pizzaPhoto.src = pizza_list[$id] + ".jpg";
         pizza_price.innerHTML = 'De prijs van uw pizza: €' + pizza_prices[$id];
+
+        var price = pizza_prices[$id];
+        allPrices.push(price);
 }
 
 // Einde 
 
 topping_list = ["Salami","Geraspte kaas","Augurk","Ui","Peper","Kruiden"];
+topping_price_list = [2.50, 1, 0.75, 0.50, 0.25, 0.25];
 
-var topping_container = document.getElementById("toppings");
+
  
 function createToppingList(){
   
   // lengte van de array berekenen
+
+  var topping_container = document.getElementById("toppings");
   var toppingListLength = topping_list.length;
   
   if(toppingListLength > 0){
@@ -77,58 +84,83 @@ function createToppingList(){
     // Het maken van het ul element  
     var myToppingList = document.createElement("ul");
 
+      myToppingList.setAttribute("id", 'toppinglist');
+
+      topping_container.appendChild(myToppingList);
+
     for(var x = 0; x < toppingListLength; x++){
 
-      myToppingList.className = "toppinglist" + x;
+    var toppingListValue = document.createTextNode(topping_list[x] + ' €' + topping_price_list[x] );
 
       var toppingListItem = document.createElement("li");
 
-      var toppingListValue = document.createTextNode(topping_list[x]);
+      toppingListItem.setAttribute("id", "li" + x);
 
       toppingListItem.appendChild(toppingListValue); 
 
       myToppingList.appendChild(toppingListItem);
 
-      document.getElementsByClassName('toppinglist' + x).onclick = function() {
-        test(this.class);
+      document.getElementById('li' + x).onclick = function() {
+        selected(this.id);
       }
     }
-
-    topping_container.appendChild(myToppingList);
   } 
 }
 
-topping_price_list = [2.50, 1, 0.75, 0.50, 0.25, 0.25];
+function selected(id){
+    document.getElementById(id).style.color = "green";
+    var unselect = id.slice(-1);
+    var unselected = unselect.slice(-1);
+    console.log(unselected);
 
-var toppingPrijs_container = document.getElementById("toppings_prijzen");
+    allPrices.push(topping_price_list[unselected]);
+
+    var sum = allPrices.reduce(add, 0);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    document.getElementById('pizza_price').innerHTML = 'De prijs van uw pizza: €' + sum ;
+}
+
+// var toppingPrijs_container = document.getElementById("toppings_prijzen");
  
-function createToppingPriceList(){
+// function createToppingPriceList(){
   
-  // lengte van de array berekenen
-  var toppingPriceListLength = topping_list.length;
+//   // lengte van de array berekenen
+//   var toppingPriceListLength = topping_list.length;
   
-  if(toppingPriceListLength > 0){
+//   if(toppingPriceListLength > 0){
     
-    // Het maken van het ul element  
-    var myPriceToppingList = document.createElement("ul");
+//     // Het maken van het ul element  
+//     var myPriceToppingList = document.createElement("ul");
 
-    for(var x = 0; x < toppingPriceListLength; x++){
+//     for(var x = 0; x < toppingPriceListLength; x++){
 
-      myPriceToppingList.className = "toppingpricelist" + x;
+//       myPriceToppingList.className = "toppingpricelist" + x;
 
-      var toppingPriceListItem = document.createElement("li");
+//       var toppingPriceListItem = document.createElement("li");
 
-      var toppingPriceListValue = document.createTextNode(topping_price_list[x]);
+//       var toppingPriceListValue = document.createTextNode(topping_price_list[x]);
 
-      toppingPriceListItem.appendChild(toppingPriceListValue); 
+//       toppingPriceListItem.appendChild(toppingPriceListValue); 
 
-      myPriceToppingList.appendChild(toppingPriceListItem);
+//       myPriceToppingList.appendChild(toppingPriceListItem);
 
-      document.getElementsByClassName('toppingpricelist' + x).onclick = function() {
-        test(this.class);
-      }
-    }
+//       document.getElementsByClassName('toppingpricelist' + x).onclick = function() {
+//         test(this.class);
+//       }
+//     }
 
-    toppingPrijs_container.appendChild(myPriceToppingList);
-  } 
-}
+//     toppingPrijs_container.appendChild(myPriceToppingList);
+//   } 
+// }
+
+
+
+
+
+
+
+
